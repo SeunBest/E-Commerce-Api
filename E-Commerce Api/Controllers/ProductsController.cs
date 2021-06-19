c4544e26bc59e63eeca91d87cb47847b3cbd036b
Oluwaseun Oyetoyan <oyetoyanoluwaseun@gmail.com>
2021-06-19 11:00:09 +0100

    Added Pagination

--- E-Commerce Api/Controllers/ProductsController.cs
+++ E-Commerce Api/Controllers/ProductsController.cs
@@ -4,6 +4,7 @@ using Core.Interfaces;
 using Core.Specifications;
 using E_Commerce_Api.Dtos;
 using E_Commerce_Api.Errors;
+using E_Commerce_Api.Helpers;
 using Infrastructure.Data;
 using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
@@ -31,12 +32,19 @@ namespace E_Commerce_Api.Controllers
         }
 
         [HttpGet]
-        public async Task<ActionResult<IReadOnlyList<ProductToReturnDto>>> GetProducts(string sort, int? brandId, int? typeId)
+        public async Task<ActionResult<Pagination<ProductToReturnDto>>> GetProducts([FromQuery]ProductSpecParams productParams)
         {
-            var spec = new ProductsWithTypesAndBrandsSpecification(sort, brandId, typeId);
+            var spec = new ProductsWithTypesAndBrandsSpecification(productParams);
+
+            var countSpec = new ProductWithFiltersForCountSpecification(productParams);
+
+            var totalItems = await _productsRepo.CountAsync(countSpec);
 
             var products = await _productsRepo.ListAsync(spec);
-            return Ok(_mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products));
+
+            var data = _mapper.Map<IReadOnlyList<Product>, IReadOnlyList<ProductToReturnDto>>(products);
+
+            return Ok(new Pagination<ProductToReturnDto>(productParams.PageIndex, productParams.PageSize, totalItems, data));
         }
 
         [HttpGet("{id}")]
